f0fa03d681b68cf8ded647c9c6fce133dc9326a9
Karen Calulo <kcalulo@gmail.com>
2022-02-07 13:34:00 +0800

    Add Splash Screen API

--- app/src/main/java/com/appscals/dish2go/MainActivity.kt
+++ app/src/main/java/com/appscals/dish2go/MainActivity.kt
@@ -2,10 +2,20 @@ package com.appscals.dish2go
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.appscals.dish2go.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        installSplashScreen()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
