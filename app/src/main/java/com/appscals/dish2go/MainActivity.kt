f468f9dd745566ff146d73e6087449f64189a27b
Karen Calulo <kcalulo@gmail.com>
2022-02-09 11:13:21 +0800

    Add Bottom Navigation Fragments and Menu

--- app/src/main/java/com/appscals/dish2go/MainActivity.kt
+++ app/src/main/java/com/appscals/dish2go/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.appscals.dish2go
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
 import com.appscals.dish2go.databinding.ActivityMainBinding
+import com.appscals.dish2go.fragment.FavoritesFragment
+import com.appscals.dish2go.fragment.HomeFragment
+import com.appscals.dish2go.fragment.MenuFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,5 +21,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        initViews()
+    }
+
+    private fun initViews() {
+
+        val firstFragment = HomeFragment()
+        val secondFragment = MenuFragment()
+        val thirdFragment = FavoritesFragment()
+
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_home -> replaceFragment(firstFragment)
+                R.id.menu_food -> replaceFragment(secondFragment)
+                R.id.menu_fave -> replaceFragment(thirdFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frame_layout, fragment)
+            commit()
+        }
     }
 }
