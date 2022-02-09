61293466aefc35c52c0ee7a5cc35a343f01ec075
Karen Calulo <kcalulo@gmail.com>
2022-02-10 02:41:55 +0800

    Add UI for Add Dish Activity, Add Custom Toolbar

--- app/src/main/java/com/appscals/dish2go/view/activity/AddUpdateDishActivity.kt
+++ app/src/main/java/com/appscals/dish2go/view/activity/AddUpdateDishActivity.kt
@@ -2,6 +2,7 @@ package com.appscals.dish2go.view.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.appscals.dish2go.R
 import com.appscals.dish2go.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
@@ -14,7 +15,17 @@ class AddUpdateDishActivity : AppCompatActivity() {
         binding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        setUpActionBar()
         initViews()
+    }
+
+    private fun setUpActionBar() {
+        binding.apply {
+            toolbar.appBarTitleTv.text = getString(R.string.add_dish_title)
+            toolbar.appBarBackIv.setOnClickListener{
+                onBackPressed()
+            }
+        }
     }
 
     private fun initViews() {
